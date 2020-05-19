29847f710ac76ab97a0b0bc943bea0f7d4a9c6f6
tarquin-the-brave <tomsteavenson@gmail.com>
2020-05-19 21:17:16 +0100

    fix modify implementation

--- vector/modify.hs
+++ vector/modify.hs
@@ -1,15 +1,15 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-12.21 script
+{-# LANGUAGE RankNTypes #-}
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as VM
 -- import Data.Vector.Unboxed (modify)
 import Control.Monad.ST
 
--- TODO: debug this...
-modify :: VM.Unbox a => (VM.MVector s a -> ST s ()) -> V.Vector a -> V.Vector a
+modify :: VM.Unbox a => (forall s. VM.MVector s a -> ST s ()) -> V.Vector a -> V.Vector a
 modify sa v = runST $ do
   mv <- V.thaw v
-  _ <- sa mv
+  sa mv
   V.freeze mv
 
 main :: IO ()
